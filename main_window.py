bd076e240fab506d057e67f5674466c685e6157b
jresmer <jpresmer@gmail.com>
2024-03-24 23:36:41 -0300

    zoom updated

--- main_window.py
+++ main_window.py
@@ -37,9 +37,6 @@ class MainWindow(WindowGUI):
     def on_mousewheel(self, multiplier: int):
         try:
             pct = int(self.__widgets["zoom txt box"].get("1.0", "end-1c"))
-            # if valor_percentual < 1 or valor_percentual > 99:
-            #     self.notify_status("Erro ao utilizar o zoom. O Step deve ser de 1% a 99%")
-            # else:
             multiplier *= pct/100
             self.__controller.zoom_window(pct=multiplier)
         except Exception as e:
