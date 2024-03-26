29b9c5e43f1c5ea3e708ecff5f1cda9ff8090293
jresmer <jpresmer@gmail.com>
2024-03-26 10:53:43 -0300

    transformation function implemented

--- canvas_object.py
+++ canvas_object.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from color import Color
+import numpy as np
 
 
 # TODO - rever necessidade de tkinte_id ser passado por parâmetro
@@ -16,7 +17,20 @@ class CanvasObject(ABC):
 
     def get_name(self):
         return self.__name
+    
+    def transform(self, coordinates: list, m: np.array) -> list:
 
+        new_coords = list()
+
+        for coordinate in self.__coord:
+
+            np_coord = np.array(coordinate)
+
+            new_coord = np.matmul(np_coord, m)
+            new_coords.append(new_coord)
+        
+        self.__coord = new_coords
+    
     @abstractmethod
     def draw(self, viewport: tuple, window: tuple) -> None:
         pass
