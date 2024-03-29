c74715c406efa7833de3caf3c9c7cdd28df6a0fa
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2024-03-29 19:01:23 -0300

    ajuste de coordenadas para ponto

--- point.py
+++ point.py
@@ -5,6 +5,7 @@ from color import Color
 class Point(CanvasObject):
     #TODO: Passar os atributos para classe pai e requisitar o atributo
     def __init__(self, coord: tuple, color: Color, name: str, tkinter_id: int, canvas) -> None:
+        coord = [(coord[0], coord[1])]
         super().__init__(coord, color, name, tkinter_id, canvas)
 
     def draw(self, viewport: tuple, window: tuple, zoom: float) -> None:
@@ -13,7 +14,7 @@ class Point(CanvasObject):
 
         window_xmin, window_ymin, window_xmax, window_ymax = window
         vp_xmin, vp_ymin, vp_xmax, vp_ymax = viewport
-        x, y = self.get_coord()
+        [(x, y)] = self.get_coord()
 
         x_vp = (x - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
         y_vp = (y - window_ymin) * (vp_ymax - vp_ymin) / (window_ymax - window_ymin)
