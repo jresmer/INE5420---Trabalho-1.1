da9c726a60a0c169fe2789eb5c90bd940f19b5d3
jresmer <jpresmer@gmail.com>
2024-04-07 12:05:20 -0300

    first commit: unfinished

---
+++ obj_descriptor.py
@@ -0,0 +1,44 @@
+from canvas_object import CanvasObject
+
+
+class OBJDescriptor:
+
+    @staticmethod
+    def obj_to_wavefront(obj: CanvasObject, path: str, overwrite: bool=True) -> None:
+
+        # writes string with obj's data
+        name = obj.get_name()
+        coords = obj.get_coord()
+        color = obj.get_color()
+
+        data = "o {}\n".format(name)
+        data += "c {}\n".format(color)
+        aux_data = ""
+
+        for i, coord in enumerate(coords):
+
+            if len(coord) > 3:
+
+                while len(coord) > 3:
+
+                    coord.append(1)
+            
+            x, y, z = coord
+            coord_data = "v {} {} {}\n".format(x, y, z)
+            data += coord_data
+
+            next_coord = i + 2 if i + 2 < len(coords) else 1
+            aux_data += "| {} {}\n".format(i + 1, next_coord)
+
+        data += aux_data + "\n"
+
+        # writes on the respective file
+        mode = "wt" if overwrite else "at"
+        with open(path, mode) as file:
+
+            file.write(data)
+
+    @staticmethod
+    def wavefront_to_obj(path: str) -> tuple:
+
+        ...
