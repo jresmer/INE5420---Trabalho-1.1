window rotation fixed

utils.py
@@ -53,20 +53,22 @@ class Utils:
         return m
     
     @staticmethod
+    def gen_simple_rotation_matrix(angle: float) -> np.array:
+
+        angle = np.radians(angle)
+        cos_teta = np.cos(angle)
+        sen_teta = np.sin(angle)
+
+        m = np.array([[cos_teta,   -sen_teta,   0],
+                        [sen_teta,   cos_teta,   0],
+                        [0,   0,    1]])
+        
+        return m
+    
+    @staticmethod
     def transform(coord: tuple, m: np.array) -> list:
 
         coord = np.array(coord + (1,))
         coord = np.matmul(coord, m)
         coord.tolist()
         return coord[:-1]
-
-    @staticmethod
-    def calc_center(coords: tuple):
-
-        n = len(coords)
-        sum_x = 0
-        sum_y = 0
-        for (x,y) in coords:
-            sum_x += x
-            sum_y += y
-        return (sum_x/n, sum_y/n)

window_coord_controller.py
@@ -18,24 +18,16 @@ class WindowCoordController:
         return np.sqrt(vx ** 2 + vy ** 2)
     
     # converts world coordinates (x, y) to normalized coordinates
-    def __world_to_normalized(self, coord: tuple, center: tuple) -> tuple:
+    def __world_to_normalized(self, coord: tuple) -> tuple:
         # translate coord in (-Wcx, -Wcy)
         dx, dy = self.__origin
         m = Utils.gen_translation_matrix(-dx, -dy)
         coord = tuple(Utils.transform(coord, m))
 
-        print(coord)
-
         # rotate coord in -θ(Y, vup)
         theta = self.get_angle(self.__vup)
-        m = Utils.gen_rotation_matrix(
-            angle=np.degrees(-theta),
-            cx=center[0],
-            cy=center[1]
-        )
+        m = Utils.gen_simple_rotation_matrix(np.degrees(-theta))
         coord = tuple(Utils.transform(coord, m))
-
-        print(coord)
 
         # normalize coord
         y_max = self.__mag(self.__vup)
@@ -43,7 +35,7 @@ class WindowCoordController:
         x, y = coord
         new_x = x / (x_max)
         new_y = y / (y_max)
-        
+
         return (new_x, new_y)
 
     def get_origin(self) -> tuple:
@@ -67,12 +59,10 @@ class WindowCoordController:
 
     def change_coords(self, name: str, coords: tuple) -> None:
         new_coords = list()
-        
-        center = Utils.calc_center(coords)
 
         # converts coordinates to the window appropriate format
         for coord in coords:
-            new_coord = self.__world_to_normalized(coord, center)
+            new_coord = self.__world_to_normalized(coord)
             new_coords.append(new_coord)
 
         self.__obj_coordinates[name] = new_coords
@@ -95,25 +85,26 @@ class WindowCoordController:
         x,y = vector
         if x != 0 and y != 0:
             alpha = np.arctan(abs(x)/abs(y))
-        elif x != 0 and y == 0:
-            alpha = np.pi/2
         else:
             alpha = 0
 
-        theta = 0
-        if x != 0 and y == 0:
-            theta = np.pi/2 if y > 0 else 3*np.pi/2
+        if x > 0 and y == 0:
+            theta = np.pi/2
+        elif x < 0 and y == 0:
+            theta = 3*np.pi/2
         elif x == 0 and y < 0:
             theta = np.pi
-
+        elif x == 0 and y > 0:
+            theta = 0
         elif x > 0 and y > 0:
-            theta = alpha
+            theta = np.pi / 2 - alpha
         elif x < 0 and y > 0:
-            theta = -alpha
+            theta = 3*np.pi / 2 + alpha
         elif x > 0 and y < 0:
-            theta = np.pi - alpha
+            theta = np.pi / 2 + alpha
         elif x < 0 and y < 0:
-            theta = np.pi + alpha
+            theta = 3*np.pi / 2 - alpha
+
         return theta
     
     def move(self, dx: int, dy: int, objs: dict) -> dict:
@@ -138,18 +129,37 @@ class WindowCoordController:
         angle = np.radians(angle)
         magnitude_v = self.__mag(self.__vup)
         magnitude_u = self.__mag(self.__u)
+
+        # print("magnitudes {}, {}\n".format(magnitude_v, magnitude_u))
         
         current_angle_v = self.get_angle(self.__vup)
-
-        ux, uy = self.__u
         current_angle_u = self.get_angle(self.__u)
+
+        # print("current angles: \n{}\n{}\n".format(np.degrees(current_angle_v), np.degrees(current_angle_u)))
     
         new_angle_v = current_angle_v + angle
         new_angle_u = current_angle_u + angle
-        self.__vup = (np.sin(new_angle_v) * magnitude_v,
-                      np.cos(new_angle_v) * magnitude_v)
-        self.__u = (np.sin(new_angle_u) * magnitude_u,
-                      np.cos(new_angle_u) * magnitude_u)
+
+        # print("new angles: \n{}\n{}\n".format(np.degrees(new_angle_v), np.degrees(new_angle_u)))
+
+        # print("vectors bfr: \n{}\n{}\n".format(self.__vup, self.__u))
+
+        vupx = np.sin(new_angle_v) * magnitude_v
+        if vupx - int(vupx) < 1E-1:
+            vupx = int(vupx)
+        vupy = np.cos(new_angle_v) * magnitude_v
+        if vupy - int(vupy) < 1E-1:
+            vupy = int(vupy)
+        self.__vup = (vupx,vupy)
+        ux = np.sin(new_angle_u) * magnitude_u
+        if ux - int(ux) < 1E-1:
+            ux = int(ux)
+        uy = np.cos(new_angle_u) * magnitude_u
+        if uy - int(uy) < 1E-1:
+            uy = int(uy)
+        self.__u = (ux, uy)
+        
+        # print("vectors after: \n{}\n{}\n".format(self.__vup, self.__u))
 
         for name in objs.keys():
 
@@ -164,9 +174,7 @@ class WindowCoordController:
         multiplier = np.sqrt(1/(1 + multiplier)) if multiplier >= 0 else np.sqrt(1 + abs(multiplier))
         magnitude_v = self.__mag(self.__vup) * multiplier
         magnitude_u = self.__mag(self.__u) * multiplier
-        vupx, vupy = self.__vup
         angle_v = self.get_angle(self.__vup)
-        ux, uy = self.__u
         angle_u = self.get_angle(self.__vup)
         self.__vup = (np.sin(angle_v) * magnitude_v,
                       np.cos(angle_v) * magnitude_v)