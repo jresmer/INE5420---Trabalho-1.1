895a2cdace34dd6bf9f01306c2d368feb2425743
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2024-04-15 11:06:01 -0300

    comentando testes anteriores

--- wireframe.py
+++ wireframe.py
@@ -18,25 +18,25 @@ class Wireframe(CanvasObject):
         tkinter_ids = []
 
         for (x1,y1) in window_coords[1:]:
-            if not ((x0 == x1 == -1) or (x0 == x1 == 1) or (y0 == y1 == -1) or (y0 == y1 == 1)):
-                x0_vp = (x0 - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
-                y0_vp = (1 - (y0 - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
-                x1_vp = (x1 - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
-                y1_vp = (1 - (y1 - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
-
-                tkinter_ids.append(self.get_canvas().create_line(x0_vp, y0_vp, x1_vp, y1_vp, fill=self.get_color(), width=zoom))
-
-                x0, y0 = x1, y1
-        
-        x0, y0 = window_coords[-1]
-        x1, y1 = window_coords[0]
-        if not ((x0 == x1 == -1) or (x0 == x1 == 1) or (y0 == y1 == -1) or (y0 == y1 == 1)):
+            # if not ((x0 == x1 == -1) or (x0 == x1 == 1) or (y0 == y1 == -1) or (y0 == y1 == 1)):
             x0_vp = (x0 - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
             y0_vp = (1 - (y0 - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
             x1_vp = (x1 - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
             y1_vp = (1 - (y1 - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
 
             tkinter_ids.append(self.get_canvas().create_line(x0_vp, y0_vp, x1_vp, y1_vp, fill=self.get_color(), width=zoom))
+
+            x0, y0 = x1, y1
+        
+        x0, y0 = window_coords[-1]
+        x1, y1 = window_coords[0]
+        # if not ((x0 == x1 == -1) or (x0 == x1 == 1) or (y0 == y1 == -1) or (y0 == y1 == 1)):
+        x0_vp = (x0 - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
+        y0_vp = (1 - (y0 - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
+        x1_vp = (x1 - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
+        y1_vp = (1 - (y1 - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
+
+        tkinter_ids.append(self.get_canvas().create_line(x0_vp, y0_vp, x1_vp, y1_vp, fill=self.get_color(), width=zoom))
         self.set_tkinter_id(tkinter_ids)
 
     def delete(self):
