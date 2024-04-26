655019c75e1739e8536a526e0b359a1fabab3cbd
jresmer <jpresmer@gmail.com>
2024-04-25 21:45:00 -0300

    CubicEquationSolver class created

--- utils.py
+++ utils.py
@@ -1,8 +1,115 @@
 import numpy as np
-import sympy as sp
+import math
 from copy import deepcopy
 from singleton_meta import SingletonMeta
-import CubicEquationSolver as ces
+
+class CubicEquationSolver:
+    # CUBIC ROOT SOLVER
+
+    # Date Created   :    24.05.2017
+    # Created by     :    Shril Kumar [(shril.iitdhn@gmail.com),(github.com/shril)] &
+    #                     Devojoyti Halder [(devjyoti.itachi@gmail.com),(github.com/devojoyti)]
+
+    # Project        :    Classified 
+    # Use Case       :    Instead of using standard numpy.roots() method for finding roots,
+    #                     we have implemented our own algorithm which is ~10x faster than
+    #                     in-built method.
+
+    # Algorithm Link :    www.1728.org/cubic2.htm
+
+    # This script (Cubic Equation Solver) is an independent program for computation of roots of Cubic Polynomials. This script, however,
+    # has no relation with original project code or calculations. It is to be also made clear that no knowledge of it's original project 
+    # is included or used to device this script. This script is complete freeware developed by above signed users, and may further be
+    # used or modified for commercial or non-commercial purpose.
+
+
+    # Libraries imported for fast mathematical computations.
+    # import math
+    # import numpy as np
+
+    # Main Function takes in the coefficient of the Cubic Polynomial
+    # as parameters and it returns the roots in form of numpy array.
+    # Polynomial Structure -> ax^3 + bx^2 + cx + d = 0
+    
+    @staticmethod
+    def solve(a, b, c, d):
+
+        # Helper function to return float value of f.
+        def findF(a, b, c):
+            return ((3.0 * c / a) - ((b ** 2.0) / (a ** 2.0))) / 3.0
+
+
+        # Helper function to return float value of g.
+        def findG(a, b, c, d):
+            return (((2.0 * (b ** 3.0)) / (a ** 3.0)) - ((9.0 * b * c) / (a **2.0)) + (27.0 * d / a)) /27.0
+
+
+        # Helper function to return float value of h.
+        def findH(g, f):
+            return ((g ** 2.0) / 4.0 + (f ** 3.0) / 27.0)
+
+        if (a == 0 and b == 0):                   # Case for handling Liner Equation
+            if c == 0: return np.array([d])
+            return np.array([(-d * 1.0) / c])                 # Returning linear root as numpy array.
+
+        elif (a == 0):                              # Case for handling Quadratic Equations
+
+            D = c * c - 4.0 * b * d                       # Helper Temporary Variable
+            if D >= 0:
+                D = math.sqrt(D)
+                x1 = (-c + D) / (2.0 * b)
+                x2 = (-c - D) / (2.0 * b)
+            else:
+                D = math.sqrt(-D)
+                x1 = (-c + D * 1j) / (2.0 * b)
+                x2 = (-c - D * 1j) / (2.0 * b)
+                
+            return np.array([x1, x2])               # Returning Quadratic Roots as numpy array.
+
+        f = findF(a, b, c)                          # Helper Temporary Variable
+        g = findG(a, b, c, d)                       # Helper Temporary Variable
+        h = findH(g, f)                             # Helper Temporary Variable
+
+        if f == 0 and g == 0 and h == 0:            # All 3 Roots are Real and Equal
+            if (d / a) >= 0:
+                x = (d / (1.0 * a)) ** (1 / 3.0) * -1
+            else:
+                x = (-d / (1.0 * a)) ** (1 / 3.0)
+            return np.array([x, x, x])              # Returning Equal Roots as numpy array.
+
+        elif h <= 0:                                # All 3 roots are Real
+
+            i = math.sqrt(((g ** 2.0) / 4.0) - h)   # Helper Temporary Variable
+            j = i ** (1 / 3.0)                      # Helper Temporary Variable
+            k = math.acos(-(g / (2 * i)))           # Helper Temporary Variable
+            L = j * -1                              # Helper Temporary Variable
+            M = math.cos(k / 3.0)                   # Helper Temporary Variable
+            N = math.sqrt(3) * math.sin(k / 3.0)    # Helper Temporary Variable
+            P = (b / (3.0 * a)) * -1                # Helper Temporary Variable
+
+            x1 = 2 * j * math.cos(k / 3.0) - (b / (3.0 * a))
+            x2 = L * (M + N) + P
+            x3 = L * (M - N) + P
+
+            return np.array([x1, x2, x3])           # Returning Real Roots as numpy array.
+
+        elif h > 0:                                 # One Real Root and two Complex Roots
+            R = -(g / 2.0) + math.sqrt(h)           # Helper Temporary Variable
+            if R >= 0:
+                S = R ** (1 / 3.0)                  # Helper Temporary Variable
+            else:
+                S = (-R) ** (1 / 3.0) * -1          # Helper Temporary Variable
+            T = -(g / 2.0) - math.sqrt(h)
+            if T >= 0:
+                U = (T ** (1 / 3.0))                # Helper Temporary Variable
+            else:
+                U = ((-T) ** (1 / 3.0)) * -1        # Helper Temporary Variable
+
+            x1 = (S + U) - (b / (3.0 * a))
+            # x2 = -(S + U) / 2 - (b / (3.0 * a)) + (S - U) * math.sqrt(3) * 0.5j
+            # x3 = -(S + U) / 2 - (b / (3.0 * a)) - (S - U) * math.sqrt(3) * 0.5j
+            
+            return np.array([x1])           # Returning One Real Root and two Complex Roots as numpy array.
 
 
 class Clipping(SingletonMeta):
@@ -321,27 +428,27 @@ class Clipping(SingletonMeta):
         #Find intersections
         # t = sp.Symbol('t', real = True)
         t_intercept = set()
-        for i in ces.solve(ax,bx,cx,dx-x_min):#sp.solveset(ax*t**3 + bx*t**2 + cx*t + dx - x_min,t):
+        for i in CubicEquationSolver.solve(ax,bx,cx,dx-x_min):#sp.solveset(ax*t**3 + bx*t**2 + cx*t + dx - x_min,t):
             x = ax*i**3 + bx*i**2 + cx*i + dx
             y = ay*i**3 + by*i**2 + cy*i + dy
             
             if i >= 0 and i <= 1 and y <= y_max and y >= y_min:
                 t_intercept.add(i)
 
-        for i in ces.solve(ax,bx,cx,dx-x_max):
+        for i in CubicEquationSolver.solve(ax,bx,cx,dx-x_max):
             x = ax*i**3 + bx*i**2 + cx*i + dx
             y = ay*i**3 + by*i**2 + cy*i + dy
             
             if i >= 0 and i <= 1 and y <= y_max and y >= y_min:
                 t_intercept.add(i)
 
-        for i in ces.solve(ay,by,cy,dy-y_min):
+        for i in CubicEquationSolver.solve(ay,by,cy,dy-y_min):
             x = ax*i**3 + bx*i**2 + cx*i + dx
             y = ay*i**3 + by*i**2 + cy*i + dy
             if i >= 0 and i <= 1 and x <= x_max and x >= x_min:
                 t_intercept.add(i)
         
-        for i in ces.solve(ay,by,cy,dy-y_max):
+        for i in CubicEquationSolver.solve(ay,by,cy,dy-y_max):
             x = ax*i**3 + bx*i**2 + cx*i + dx
             y = ay*i**3 + by*i**2 + cy*i + dy
             
