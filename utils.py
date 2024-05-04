rotation fixed

utils.py
@@ -579,9 +579,9 @@ class Utils:
 
         if return_angles:
 
-            return m1, alpha, theta
+            return m, alpha, theta
         
-        return m1
+        return m
     
     @staticmethod
     def gen_3d_rotation_matrix(angle: float, rotation_axis: tuple) -> np.array:
@@ -596,7 +596,8 @@ class Utils:
         # finding vector D:
         # rotation axis is defined by a point P and a vector A
         p, a = rotation_axis
-        dx, dy, dz  = p
+        print(f"rotation axis={rotation_axis}")
+        dx, dy, dz = p
         m1 = np.array([[1, 0, 0, 0],
                       [0, 1, 0, 0],
                       [0, 0, 1, 0],
@@ -610,6 +611,8 @@ class Utils:
         rotation axis is aligned with the y axis
         """
         m23, alpha, theta = Utils.rotation_to_y_axis_matrix(rotation_axis, return_angles=True)
+
+        print(f"0x={np.degrees(theta)}, 0z={np.degrees(alpha)}")
 
         """
         4. Rotate the object around the y axis in the intended angle