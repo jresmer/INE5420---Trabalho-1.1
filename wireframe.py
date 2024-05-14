da5bb0bb477d871cf3d14afe5a32c4cf6696c11c
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2024-05-14 19:12:40 -0300

    ajuste wavefront

--- wireframe.py
+++ wireframe.py
@@ -34,10 +34,10 @@ class Wireframe(CanvasObject):
 
             #     (x0_vp,y0_vp),(x1_vp,y1_vp) = new_coords
             if not ((x0 == x1 == 1) or (x0 == x1 == -1) or (y0 == y1 == 1) or (y0 == y1 == -1)):
-                x0_vp = vp_xmin + (x0_vp - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
-                y0_vp = vp_ymin + (1 - (y0_vp - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
-                x1_vp = vp_xmin + (x1_vp - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
-                y1_vp = vp_ymin + (1 - (y1_vp - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
+                x0_vp = vp_xmin + (x0 - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
+                y0_vp = vp_ymin + (1 - (y0 - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
+                x1_vp = vp_xmin + (x1 - window_xmin) * (vp_xmax - vp_xmin) / (window_xmax - window_xmin)
+                y1_vp = vp_ymin + (1 - (y1 - window_ymin)/(window_ymax - window_ymin)) * (vp_ymax - vp_ymin)
 
                 tkinter_ids.append(self.get_canvas().create_line(x0_vp, y0_vp, x1_vp, y1_vp, fill=self.get_color(), width=zoom))
 
