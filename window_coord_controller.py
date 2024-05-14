3717dc6b786eadf57112ee7ddb05e13fa37a6bef
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2024-05-14 12:49:07 -0300

    ajuste projeção

--- window_coord_controller.py
+++ window_coord_controller.py
@@ -16,7 +16,7 @@ class WindowCoordController:
         self.angleY = 0
         self.angleZ = 0
         self.__proj_m = Utils.get_perspective_projection_matrix(self.__cop, self.angleX, self.angleY)
-        self.__zw_projected = Utils.transform(self.__origin, self.__proj_m)[-1]
+        self.__zw_projected = abs(p[2] - cop[2])
 
     @staticmethod
     def __mag(v: tuple) -> float:
@@ -29,9 +29,7 @@ class WindowCoordController:
         return np.sqrt(sum_)
     
     def __recalculate_projection_matrix(self):
-
         self.__proj_m = Utils.get_perspective_projection_matrix(self.__cop, self.angleX, self.angleY)
-        self.__zw_projected = Utils.transform(self.__origin, self.__proj_m)[-1]
     
     # converts world coordinates (x, y) to normalized coordinates
     def __world_to_normalized(self, coord: tuple) -> tuple:
@@ -75,19 +73,19 @@ class WindowCoordController:
 
         for coord in coords:          
             x, y, z = tuple(Utils.transform(coord, self.__proj_m))
-            
+
             d = self.__zw_projected
 
-            x = x/(z/d)
-            y = y/(z/d)
- 
             if z < d:
                 new_coords = []
                 break
 
+            x = x/(z/d)
+            y = y/(z/d)
+
             new_coord = self.__world_to_normalized((x,y))
             new_coords.append(new_coord)
-        print(new_coords)
+
         self.__obj_coordinates[name] = new_coords
 
     # adds new obj coordinates 
@@ -165,7 +163,6 @@ class WindowCoordController:
         self.__vup = tuple(Utils.transform(self.__vup, m))
         self.__u = tuple(Utils.transform(self.__u, m))
         self.__vpn = tuple(Utils.transform(self.__vpn, m))
-        
 
         self.__recalculate_projection_matrix()
         self.update_coordinates(objs)
