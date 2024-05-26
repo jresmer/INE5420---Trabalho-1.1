e9e49f07ea369bb6340f3c5da085fd25c2d80356
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2024-05-26 16:49:50 -0300

    retirado curve_clipping em bsplinesurface

--- bspline_surface.py
+++ bspline_surface.py
@@ -24,41 +24,26 @@ class BSplineSurface(CanvasObject):
 
     def draw(self, viewport: tuple, window_coords: tuple, zoom: float) -> None:
 
-        def aux(viewport, n, DDx_row, DDy_row, vx, vy, ids, coords):    
-
-            vp_xmin, vp_ymin, vp_xmax, vp_ymax = viewport
+        def aux(viewport, n, DDx_row, DDy_row, ids):    
 
             x0, delta_x, delta_x2, delta_x3 = DDx_row
             y0, delta_y, delta_y2, delta_y3 = DDy_row
 
-            intercept = Clipping.curve_clipping(viewport, coords, vx, vy)
+            for _ in range(n):
+                x1 = x0 + delta_x
+                delta_x = delta_x + delta_x2
+                delta_x2 = delta_x2 + delta_x3
 
-            draw = False
-            if vp_xmin <= x0 and x0 <= vp_xmax and vp_ymin <= y0 and y0 <= vp_ymax:
-                draw = True
+                y1 = y0 + delta_y
+                delta_y = delta_y + delta_y2
+                delta_y2 = delta_y2 + delta_y3
 
-            intercept = [int(i*n) for i in intercept]
-
-            for i in range(len(intercept)-1):
-                lim_inf = intercept[i]
-                lim_sup = intercept[i+1]
-
-                for _ in range(lim_inf,lim_sup + 1):
-
-                    x1 = x0 + delta_x
-                    delta_x = delta_x + delta_x2
-                    delta_x2 = delta_x2 + delta_x3
-
-                    y1 = y0 + delta_y
-                    delta_y = delta_y + delta_y2
-                    delta_y2 = delta_y2 + delta_y3
-
-                    if draw:
-                        tk_id = self.get_canvas().create_line(x0, y0, x1, y1, fill=self.get_color())
-                        ids.append(tk_id)
-                
-                    x0, y0 = x1,y1
-                draw = not draw
+                new_coords = Clipping.liang_barsky(viewport, [(x0,y0),(x1,y1)])
+                if new_coords:
+                    tk_id = self.get_canvas().create_line(*new_coords, fill=self.get_color())
+                    ids.append(tk_id)
+            
+                x0, y0 = x1,y1
 
         # initializing ids list
         tk_ids = list()
@@ -89,7 +74,6 @@ class BSplineSurface(CanvasObject):
             for j in range(4):
                 Gx[i][j] = window_coords[i*4+j][0]
                 Gy[i][j] = window_coords[i*4+j][1]
-
 
         m = Utils.get_m_bspline()
         mt = np.transpose(m)
@@ -131,21 +115,11 @@ class BSplineSurface(CanvasObject):
         # starting conditions for the curves in s (6, 7)
         DDx_, DDy_ = deepcopy(np.transpose(DDx)), deepcopy(np.transpose(DDy))
 
-
-
         """
         5. Draw the curve family in t
         """
-        range_ = 1/self.__steps
         for i in range(self.__steps+1):
-            
-            s = range_*i
-            s2 = s*s
-            s3 = s2*s
-            vx = np.matmul([s3,s2,s,1], Cx)
-            vy = np.matmul([s3,s2,s,1], Cy)
-
-            aux(viewport, self.__steps, DDx[0], DDy[0], vx, vy, tk_ids, window_coords)
+            aux(viewport, self.__steps, DDx[0], DDy[0], tk_ids)
             for j in range(len(DDx)-1):
                 DDx[j] = np.array(DDx[j]) + np.array(DDx[j+1])
                 DDy[j] = np.array(DDy[j]) + np.array(DDy[j+1])
@@ -154,14 +128,7 @@ class BSplineSurface(CanvasObject):
         6. Draw the curve family in s
         """
         for i in range(self.__steps+1):
-
-            t = range_*i
-            t2 = t*t
-            t3 = t2*t
-            vx = np.matmul([t3,t2,t,1], Cx)
-            vy = np.matmul([t3,t2,t,1], Cy)
-
-            aux(viewport, self.__steps, DDx_[0], DDy_[0], vx, vy, tk_ids, window_coords)
+            aux(viewport, self.__steps, DDx_[0], DDy_[0], tk_ids)
             for j in range(len(DDx_)-1):
                 DDx_[j] = np.array(DDx_[j]) + np.array(DDx_[j+1])
                 DDy_[j] = np.array(DDy_[j]) + np.array(DDy_[j+1])
